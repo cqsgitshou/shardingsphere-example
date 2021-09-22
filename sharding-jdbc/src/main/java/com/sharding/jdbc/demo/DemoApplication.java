25e56a3742abf202d299f02fbd934d24383bb9b2
qingsong <843765238@qq.com>
2021-09-22 16:57:41 +0800

    Dockerfile

--- sharding-jdbc/src/main/java/com/sharding/jdbc/demo/DemoApplication.java
+++ sharding-jdbc/src/main/java/com/sharding/jdbc/demo/DemoApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author cqs
+ */
 @SpringBootApplication
 @MapperScan("com.sharding.jdbc.demo.mapper")
 public class DemoApplication {
